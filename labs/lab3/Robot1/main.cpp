cad6281ae13aef16d598c1dee915547930e5a9eb
Andrey Dudin <andrewdudin1337@gmail.com>
2022-02-27 14:23:07 +0300

    Robot1. Начал работу на лаб. работой 3.2 Робот 1 Обработал частные случаи.

---
+++ labs/lab3/Robot1/main.cpp
@@ -0,0 +1,82 @@
+/*
+ * Программа была написана Дудиным Андреем, студентом группы ПС-21
+ * Задание 3.2
+ * ===============================================================
+ * Робот находится на расстоянии X шагов от цели. За один ход он
+ * продвигается либо к цели, либо в противоположном направлении.
+ * Если он достигает цели, то немедленно останавливается.
+ * Найти количество способов достижения цели ровно за Y шагов.
+ * ===============================================================
+ * Версия c++: 20
+ * Компиляторы: gcc(9.3.0), g++(9.3.0)
+ * ОС: Linux, Ubuntu 20.04
+ * Доподнительно: сборка происходила через gnu make(4.2.1)
+ */
+
+#include <fstream>
+#include <iostream>
+
+void initFiles(std::ifstream& input, std::ofstream& output);
+
+int main()
+{
+    std::ifstream input;
+    std::ofstream output;
+
+    try
+    {
+        int targetDistance = 0;
+        int stepsNumber = 0;
+        initFiles(input, output);
+
+        input >> targetDistance >> stepsNumber;
+        if (targetDistance < 0 || targetDistance > 50 || stepsNumber < 0 || stepsNumber > 50)
+        {
+            throw std::invalid_argument("Error, width and height should be in range 1 .. 50");
+        }
+
+        if (targetDistance == stepsNumber)
+        {
+            output << "1" << std::endl;
+            return 0;
+        }
+
+        if (targetDistance > stepsNumber)
+        {
+            output << "0" << std::endl;
+            return 0;
+        }
+
+        if (!output.flush())
+        {
+            throw std::runtime_error("Error, can't flush output");
+        }
+
+        output.close();
+    }
+    catch (const std::exception& exception)
+    {
+        std::cout << "Message: " << exception.what() << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
+
+void initFiles(std::ifstream& input, std::ofstream& output)
+{
+    const std::string INPUT_FILE_NAME = "INPUT.TXT";
+    const std::string OUTPUT_FILE_NAME = "OUTPUT.txt";
+
+    input.open(INPUT_FILE_NAME, std::ios_base::in);
+    output.open(OUTPUT_FILE_NAME, std::ios_base::out);
+    if (!input.is_open())
+    {
+        throw std::invalid_argument("Error, can't open INPUT.TXT");
+    }
+
+    if (!output.is_open())
+    {
+        throw std::invalid_argument("Error, can't open OUTPUT.TXT");
+    }
+}
